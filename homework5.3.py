29b574974270908f7ec7b8b8294e9010b74fcc72
Nataliia <ggg@>
2025-05-23 22:48:51 +0200

    commit5

---
+++ homework5.3.py
@@ -0,0 +1,9 @@
+import string
+text = input("Write your text " )
+cleaned_text = ''.join(char for char in text if char.isalpha() or char.isspace())
+words = cleaned_text.split()
+capital_letters = [item.title() for item in words]
+hashtag = "#" + "".join(capital_letters)
+if len(hashtag) > 140:
+    hashtag = hashtag[:140]
+print(hashtag)

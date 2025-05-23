3fa5021987308d0b04a836d1c30d0c00a30da359
Nataliia <ggg@>
2025-05-24 00:50:27 +0200

    commit5

--- homework5.3.py
+++ homework5.3.py
@@ -1,5 +1,5 @@
 text = input("Write your text " )
-cleaned_text = ''.join(char for char in text if char.isalpha() or char.isspace())
+cleaned_text = "".join(char for char in text if char.isalpha() or char.isspace())
 words = cleaned_text.split()
 capital_letters = [item.title() for item in words]
 hashtag = "#" + "".join(capital_letters)

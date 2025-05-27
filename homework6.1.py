8ca583937b3494e128a9ec3980b8a8fe56645a1b
Nataliia <ggg@>
2025-05-27 19:07:17 +0200

    commit6

---
+++ homework6.1.py
@@ -0,0 +1,12 @@
+import string
+example = input("Enter your example: ")
+example = example.strip()
+start, end = example.split('-')
+start_index = string.ascii_letters.index(start)
+end_index = string.ascii_letters.index(end)
+if start_index <= end_index:
+    result = string.ascii_letters[start_index:end_index+1]
+else:
+    result = string.ascii_letters[start_index:end_index - 1:-1]
+
+print(result)

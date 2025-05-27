56550e2faffc7089bb7348ae9eebe955b907d15c
Nataliia <ggg@>
2025-05-27 19:09:58 +0200

    commit6

--- homework6.1.py
+++ homework6.1.py
@@ -5,6 +5,7 @@ example = example.strip()
 start, end = example.split('-')
 start_index = string.ascii_letters.index(start)
 end_index = string.ascii_letters.index(end)
+
 if start_index <= end_index:
     result = string.ascii_letters[start_index:end_index+1]
 else:
